4a105b5a7d1aed7920cceb480fafeacf554e7b60
EL_Abhinav <56501876+ELLIPSIS009@users.noreply.github.com>
2021-09-05 18:41:38 +0530

    commit

--- Day_1/Maximum Depth of Binary Tree/Maximum Depth of Binary Tree.py
+++ Day_1/Maximum Depth of Binary Tree/Maximum Depth of Binary Tree.py
@@ -2,7 +2,7 @@
 Problem :- Maximum Depth of Binary Tree
 Link :- https://leetcode.com/problems/maximum-depth-of-binary-tree/
 
-Problem statement:- Given the root of a binary tree, return its maximum depth.
+Problem statement :- Given the root of a binary tree, return its maximum depth.
 A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 
 Example 1:
--- Day_2/Div.64/Div.64_M-2.py
+++ Day_2/Div.64/Div.64_M-2.py
@@ -1,3 +1,22 @@
+'''
+Question code :- 887A
+Platform :- Codeforces
+Link :- https://codeforces.com/contest/1446/problem/A
+
+Problem statement :- Top-model Izabella participates in the competition. She wants to impress judges and show her mathematical skills. 
+Her problem is following: for given string, consisting of only 0 and 1, tell if it's possible to remove some digits in such a way, 
+that remaining number is a representation of some positive integer, divisible by 64, in the binary numerical system.
+
+Example 1:
+input : 100010001
+output : yes
+
+Example 2:
+input : 100
+output : no
+'''
+
+
 def cal(a):
     oneFound = False
     zeroCount = 0
