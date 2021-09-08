42c8ba631f73c09bf90437c99fd74ea9a8c2fd88
EL_Abhinav <56501876+ELLIPSIS009@users.noreply.github.com>
2021-09-08 23:15:32 +0530

    commit

---
+++ Day_3/Air_Conditioner/air_conditioner.py
@@ -0,0 +1,9 @@
+def compare(p, q):
+    return p[0] < q[0]
+
+q = int(input())
+for i in range(q):
+    n ,m = list(map(int, input().split()))
+    P = list(map(int, input().split()))
+    for j in range(n):
+        
