c2700995a2019568c19f4f4976f028879ed25942
EL_Abhinav <56501876+ELLIPSIS009@users.noreply.github.com>
2021-09-14 22:57:53 +0530

    commit

---
+++ Day_3/Increasing_Order_Search_Tree/search_tree.py
@@ -0,0 +1,44 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def inorder(root):
+        tree = []
+        if root:
+            Solution.inorder(root.left)
+            tree.append(root.val)
+            Solution.inorder(root.right)
+        return tree
+
+    def increasingBST(root):
+        if root is None: 
+            return root
+        elif root.right == None and root.left == None:
+            return root
+        else:
+            temp = TreeNode()
+            tree = Solution.inorder(root)
+            for i in range(len(tree)):
+                if i == 0:
+                    z = TreeNode(tree[i])
+                    temp = z
+                    root = z
+                else:
+                    z = TreeNode(tree[i])
+                    temp.right = z
+                    temp = z
+        return root
+
+root = TreeNode(5)
+root.left = TreeNode(3)
+root.right = TreeNode(6)
+root.left.left = TreeNode(2)
+root.left.right = TreeNode(4)
+root.left.left.left = TreeNode(1)
+root.right.right = TreeNode(8)
+root.right.right.left = TreeNode(7)
+root.right.right.right = TreeNode(9)
+print(Solution.inorder(Solution.increasingBST(root)))
