45cafddf2fabf50403917d144e4b203c028a485a
EL_Abhinav <56501876+ELLIPSIS009@users.noreply.github.com>
2021-09-15 10:12:12 +0530

    commit

--- Day_3/Swap_Nodes_in_Pairs/swap.py
+++ Day_3/Swap_Nodes_in_Pairs/swap.py
@@ -3,6 +3,22 @@ class ListNode:
         self.val = val
         self.next = next
 class Solution:
-    def swapPairs(self, root):
-        pass
+    def tree(root):
+        tree = []
+        if root:
+            tree.append(root.val)
+            tree += Solution.tree(root.next)
+        return tree
+
+    def swapPairs(root):
+        tree = Solution.tree(root)
+        for i in range(0, len(tree), 2):
+            tree[i], tree[i+1] = tree[i+1], tree[i]
+        return tree
+
+root = ListNode(1)
+root.next = ListNode(2)
+root.next.next = ListNode(3)
+root.next.next.next = ListNode(4)
+print(Solution.swapPairs(root))
         
