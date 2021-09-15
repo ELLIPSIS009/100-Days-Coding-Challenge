acd0eea0c3fde529ceb3d49dd5c3956c467efb4f
EL_Abhinav <56501876+ELLIPSIS009@users.noreply.github.com>
2021-09-15 22:10:01 +0530

    commit

--- Day_3/Swap_Nodes_in_Pairs/swap.py
+++ Day_3/Swap_Nodes_in_Pairs/swap.py
@@ -11,14 +11,22 @@ class Solution:
         return tree
 
     def swapPairs(root):
-        tree = Solution.tree(root)
-        for i in range(0, len(tree), 2):
-            tree[i], tree[i+1] = tree[i+1], tree[i]
-        return tree
+        temp = ListNode()
+        temp.next = root
+        current = temp
+        while current.next is not None and current.next.next is not None:
+            first = current.next
+            second = current.next.next
+            first.next = second.next
+            current.next = second
+            current.next.next = first
+            current = current.next.next
+        return temp.next
+
 
 root = ListNode(1)
 root.next = ListNode(2)
 root.next.next = ListNode(3)
 root.next.next.next = ListNode(4)
-print(Solution.swapPairs(root))
-        
+
+print(Solution.tree(Solution.swapPairs(root)))
